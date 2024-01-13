6d51ccb59d381c29d393258cb7cc969e388a17a8
bangoo040993 <b40n50@gmail.com>
2024-01-13 09:13:06 -0500

    how do you do this again?

--- 2024/January24/JavaScript-Array-Methods-and-Properties/at.js
+++ 2024/January24/JavaScript-Array-Methods-and-Properties/at.js
@@ -1,14 +1,14 @@
 //AT() method returns the element at the specified index in an array.
 // EXAMPLE: AT() METHOD //
 
-const brakeline = "-------------------------------"
+const breakLine = "-------------------------------"
 
 //at() method using an array of of numbers
 const array = [10, 20, 30, 40]; // An array of numbers
 console.log(array.at(2)); // Outputs: 30
 console.log(array.at(-1)); // Outputs: 40
 console.log(array.at(-5)); // Outputs: undefined
-console.log("🚀 ~ file: at.js:5 ~ brakeline:", brakeline)
+console.log("🚀 ~ file: at.js:5 ~ breakLine:", breakLine)
 
 
 //at() method using a strings
@@ -18,7 +18,7 @@ const string = "Hello World"; // A string
 console.log(string.at(2)); // Outputs: l
 console.log(string.at(-1)); // Outputs: d
 console.log(string.at(-58)); // Outputs: undefined
-console.log("🚀 ~ file: at.js:5 ~ brakeline:", brakeline)
+console.log("🚀 ~ file: at.js:5 ~ breakLine:", breakLine)
 
 //at() method if left empty it will return the first element or at(0)
 console.log(array.at(0)) // Outputs: 10 
